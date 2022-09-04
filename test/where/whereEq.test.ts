b70340a8feefe54b67ec635b63d0b5a86075ad67
Erik Soehnel <erik.soehnel@gmail.com>
2022-09-04 11:53:00 +0200

    more whereEq tests

--- test/where/whereEq.test.ts
+++ test/where/whereEq.test.ts
@@ -1,5 +1,5 @@
 import { query } from '../../src'
-import { client, Systems, Games } from '../helpers'
+import { client, Games, Manufacturers, Systems } from '../helpers'
 
 describe('whereEq', () => {
   test('number', async () => {
@@ -35,87 +35,27 @@ describe('whereEq', () => {
     ])
   })
 
-  // test('equals', async () => {
-  //   const result = await query(users.select('userName'))
-  //     .whereEq(users.userId, 'id')
-  //     .fetch(client, {
-  //       id: 2,
-  //     })
-  //
-  //   expect(result).toEqual([{ userName: 'user-c' }])
-  // })
-  //
-  // test('multiple equals', async () => {
-  //   const result = await query(users.select('userName'))
-  //     .whereEq(users.userId, 'id')
-  //     .whereEq(users.userName, 'userName')
-  //     .fetch(client, {
-  //       id: 2,
-  //       userName: 'user-c',
-  //     })
-  //
-  //   expect(result).toEqual([{ userName: 'user-c' }])
-  // })
-  //
-  // test('empty resultset', async () => {
-  //   const result = await query(users.select('userName'))
-  //     .whereEq(users.userId, 'id')
-  //     .fetch(client, {
-  //       id: 123,
-  //     })
-  //
-  //   expect(result).toEqual([])
-  // })
-  //
-  // test('is null', async () => {
-  //   const result = await query(users.select('userName', 'userAvatar'))
-  //     .whereEq(users.userAvatar, 'avatar')
-  //     .fetch(client, {
-  //       avatar: null,
-  //     })
-  //
-  //   expect(result).toEqual([
-  //     { userName: 'user-a', userAvatar: null },
-  //     { userName: 'user-c', userAvatar: null },
-  //   ])
-  // })
-  //
-  // test('anyParam', async () => {
-  //   const userQuery = await query(users)
-  //     .whereEq(users.userId, 'id')
-  //     .whereEq(users.userActive, 'userActive')
-  //
-  //   expect(
-  //     (
-  //       await userQuery.fetch(client, {
-  //         id: query.anyParam,
-  //         userActive: query.anyParam,
-  //       })
-  //     )
-  //       .map(r => r.userId)
-  //       .sort(),
-  //   ).toEqual([1, 2, 3])
-  //
-  //   expect(
-  //     (
-  //       await userQuery.fetch(client, {
-  //         id: 2,
-  //         userActive: query.anyParam,
-  //       })
-  //     )
-  //       .map(r => r.userId)
-  //       .sort(),
-  //   ).toEqual([2])
-  //
-  //   expect(
-  //     (
-  //       await userQuery.fetch(client, {
-  //         id: query.anyParam,
-  //         userActive: null,
-  //       })
-  //     )
-  //       .map(r => r.userId)
-  //       .sort(),
-  //   ).toEqual([1, 2])
-  // })
+  test('anyParam', async () => {
+    const q = query(Manufacturers)
+      .select(Manufacturers.include('name'))
+      .whereEq(Manufacturers.id, 'id')
+
+    expect(await q.fetch(client, { id: 1 })).toEqual([{ name: 'Sega' }])
+    expect(await q.fetch(client, { id: query.anyParam })).toEqual([
+      { name: 'Sega' },
+      { name: 'Nintendo' },
+      { name: 'Atari' },
+    ])
+  })
+
+  test('two conditions', async () => {
+    const q = query(Systems)
+      .select(Systems.include('name'))
+      .whereEq(Systems.manufacturerId, 'manufacturerId')
+      .whereEq(Systems.year, 'year')
+
+    expect(await q.fetch(client, { manufacturerId: 1, year: 1990 })).toEqual([
+      { name: 'Game Gear' },
+    ])
+  })
 })
